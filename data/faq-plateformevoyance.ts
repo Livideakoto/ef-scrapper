76d8cc645385230e20f3ad816af414e9f498e5fe
Livide AKOTO <livideakoto@gmail.com>
2025-07-26 16:49:50 +0000

    feat: Add FAQ data for various divination services including tarot and immediate clairvoyance

---
+++ data/faq-plateformevoyance.ts
@@ -0,0 +1,31 @@
+export interface IFaqItem {
+    title: string;
+    content: string;
+}
+
+const PlateformeVoyance = [
+    {
+        "title": "Plateforme de voyance : voyance par téléphone sérieuse et immédiate par Carte Bancaire",
+        "content": "<div><div>\n                                            <div><p>L’avenir vous pose question ? Pour consulter un voyant sur notre site, c’est très simple ! Cliquez sur le <span style=\"color: #008000;\"><strong>BOUTON VERT</strong></span> pour sélectionner le voyant de votre choix sur notre site. Si le <strong><span style=\"color: #ff6600;\">BOUTON ORANGE</span></strong> s’affiche, le voyant est en consultation tandis qu’avec le <strong><span style=\"color: #808080;\"><span style=\"color: #333333;\">BOUTON GRIS</span></span></strong>, le voyant est hors ligne. Vous pouvez visualiser le planning de chaque voyant sur le site en cliquant sur son profil directement. Sur notre plateforme, vous maîtrisez votre budget ! Choisissez votre forfait de voyance et votre communication se coupe automatiquement. Aucune surprise ou dépassement possible sur notre site. Nous comptons actuellement une équipe d’environ<strong>&nbsp;<span style=\"color: #535353;\">3</span>5 voyants visibles</strong> sur notre plateforme. Ce service est disponible<strong> 24h-24 et 7J/7</strong>, suivant la disponibilité des voyants.<strong>&nbsp;</strong></p>\n<p>&nbsp;</p>\n</div>\n                                        </div></div>"
+    },
+    {
+        "title": "Un centre de voyance par téléphone reconnu pour son sérieux",
+        "content": "<div><div>\n                                            <div><p>À la fois gage d’accessibilité et de fiabilité, ma plateforme de voyance en ligne vous permet de prendre contact avec le professionnel de la voyance de votre choix pour bénéficier d’une consultation de qualité. J’ai sélectionné pour vous des professionnels de la voyance sérieux et honnêtes, aux grandes qualités humaines et qui offrent leurs services à des prix accessibles. Tous les voyants de l’équipe ont été recrutés avec soin pour vous assurer des consultations de qualité et vous permettre de dissiper vos doutes et d’apaiser vos peurs. Pas de frais cachés, pas de dépassement et aucun surcoût à la minute. Une question ? Vous avez tout de suite la possibilité de vous faire guider par un voyant de notre équipe pour anticiper votre avenir.</p>\n<p>&nbsp;</p>\n</div>\n                                        </div></div>"
+    },
+    {
+        "title": "Notre plateforme de voyance en ligne fiable et accessible",
+        "content": "<div><div>\n                                            <div><p>Si vous souhaitez en savoir plus sur mon équipe de professionnels de la voyance, n’hésitez pas à visiter leur profil pour découvrir leur parcours et leurs compétences. Ces spécialistes de la voyance sont tous des professionnels des arts divinatoires. Ils vous proposent leurs services dans le but d’éclairer votre devenir. Pour consulter un voyant de l’équipe, rien de plus simple ! Il vous suffit de cliquer sur le bouton vert pour signifier votre choix de voyant et de vous laisser guider.</p>\n</div>\n                                        </div></div>"
+    },
+    {
+        "title": "Une plateforme de voyance sérieuse au service de tous",
+        "content": "<div><div>\n                                            <div><p>Vous avez choisi votre voyant ? Il vous faut maintenant créer un compte et sélectionner un forfait pour votre consultation. Une fois le paiement validé par notre serveur, vous recevez un code à 6 chiffres qui vous permet de débuter la consultation en toute confidentialité. Lorsque vos minutes sont épuisées, la communication prend fin automatiquement. Ma plateforme de <u><a href=\"https://www.esteban-frederic.fr/voyance-en-ligne/\">voyance en ligne</a></u>, a été pensée pour que tout se fasse dans la simplicité.</p>\n</div>\n                                        </div></div>"
+    },
+    {
+        "title": "Fiabilité et professionnalisme pour une voyance en ligne sérieuse",
+        "content": "<div><div>\n                                            <div><p>En tant que consultant, ma plateforme de voyance immédiate vous permet de trouver l’offre de voyance la plus adaptée à vos besoins et à votre budget, pour obtenir des réponses rapides à vos interrogations. Les voyants vous accompagnent et vous guident afin de vous apporter les réponses les plus sérieuses, en toute confiance. Je mise sur les compétences des voyants de mon équipe pour vous guider dans les grandes étapes de votre vie. Tous les professionnels présents sur la plateforme disposent d’une grande expérience des arts divinatoires. Soigneusement sélectionnés et testés, ils possèdent de grandes qualités humaines afin de vous accompagner au mieux sur votre chemin de vie. Ils respectent <strong>la charte de déontologie de l’INAD (Institut National des Arts Divinatoires) et vous attendent pour des consultations de voyance de qualité.&nbsp;</strong></p>\n</div>\n                                        </div></div>"
+    }
+];
+
+export const faqs: Record<string, IFaqItem[]> = {
+    "/plateforme-voyance": PlateformeVoyance,
+};
